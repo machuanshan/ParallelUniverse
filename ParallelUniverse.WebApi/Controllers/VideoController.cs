9d51f1b5d8a4573e348f2b2af041974d7a675403
chunanshan ma <machuanshan@gmail.com>
2020-09-13 21:46:08 +0800

    make watching online work

--- ParallelUniverse.WebApi/Controllers/VideoController.cs
+++ ParallelUniverse.WebApi/Controllers/VideoController.cs
@@ -4,8 +4,10 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 using ParallelUniverse.Model;
 
 namespace ParallelUniverse.WebApi.Controllers
@@ -34,6 +36,18 @@ namespace ParallelUniverse.WebApi.Controllers
             {
                 Name = Path.GetFileName(f)
             });
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetVideo(string name)
+        {
+            var dir = _configuration.GetValue<string>("VideoDir");
+            var path = Path.Combine(dir, name);
+            return new PhysicalFileResult(path, "application/octet-stream")
+            {
+                FileDownloadName = name,
+                EnableRangeProcessing = true
+            };
         }
     }
 }

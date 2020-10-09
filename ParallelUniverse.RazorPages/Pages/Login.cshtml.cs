b63750dc1ae6bc00cdfa59af1a62f7ede85fe867
Chunanshan.Ma <Chuanshan.Ma@igt.com>
2020-10-09 17:20:03 +0800

    bind property

--- ParallelUniverse.RazorPages/Pages/Login.cshtml.cs
+++ ParallelUniverse.RazorPages/Pages/Login.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -12,7 +13,14 @@ namespace ParallelUniverse.RazorPages.Pages
     {
         private readonly ParallelUniverseContext _puc;
 
+        [BindProperty]
+        [Required]
+        [StringLength(50)]
         public string UserName { get; set; }
+
+        [BindProperty]
+        [Required]
+        [StringLength(50, MinimumLength = 4)]
         public string Password { get; set; }
 
         public LoginModel(ParallelUniverseContext puc)

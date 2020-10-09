51852ae2f2d37bd96091bfebeb83c7484bf6194e
Chunanshan.Ma <Chuanshan.Ma@igt.com>
2020-10-09 15:55:04 +0800

    add log

--- ParallelUniverse.WebApi/Controllers/AccountController.cs
+++ ParallelUniverse.WebApi/Controllers/AccountController.cs
@@ -1,14 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http.Headers;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Net.Http.Headers;
 using ParallelUniverse.Model;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace ParallelUniverse.WebApi.Controllers
 {

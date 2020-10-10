a8bf746d6abfff831a36dbc2661839178683cc54
chunanshan ma <machuanshan@gmail.com>
2020-10-10 16:57:39 +0800

    finish reslist page

--- ParallelUniverse.RazorPages/Pages/ResList.cshtml.cs
+++ ParallelUniverse.RazorPages/Pages/ResList.cshtml.cs
@@ -1,33 +1,35 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
-using ParallelUniverse.RazorPages.Models;
 
 namespace ParallelUniverse.RazorPages.Pages
 {
     public class ResListModel : PageModel
     {
-        private IConfiguration _configuration;
+        private readonly IConfiguration _configuration;
 
         public List<FileInfo> FileInfos { get; set; }
+        public string[] Directories { get; set; }
+        public string CurrentDirectory { get; set; }
 
         public ResListModel(IConfiguration configuration)
         {
             _configuration = configuration;
         }
 
-        public void OnGet()
+        public void OnGet(string dir)
         {
-            var curDir = _configuration.GetValue<string>("ResourceRoot");
+            CurrentDirectory = string.IsNullOrEmpty(dir) ? 
+                _configuration.GetValue<string>("ResourceRoot") : dir;
+
             FileInfos = Directory
-                .GetFiles(curDir)
+                .GetFiles(CurrentDirectory)
                 .Select(f => new FileInfo(f))
                 .ToList();
+
+            Directories = Directory.GetDirectories(CurrentDirectory);
         }
     }
 }

7568b37b06012b269bd20cc588e4e1fb8b547c9b
chunanshan ma <machuanshan@gmail.com>
2020-10-10 17:22:28 +0800

    save resource to db

--- ParallelUniverse.RazorPages/Pages/PubRes.cshtml.cs
+++ ParallelUniverse.RazorPages/Pages/PubRes.cshtml.cs
@@ -4,35 +4,64 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ParallelUniverse.RazorPages.Data;
 using ParallelUniverse.RazorPages.Models;
 
 namespace ParallelUniverse.RazorPages.Pages
 {
     public class PubResModel : PageModel
     {
+        private readonly ParallelUniverseContext _puctx;
+
         [BindProperty]
         public FileResource FileResource { get; set; }
 
-        public void OnGet(string path)
+        public PubResModel(ParallelUniverseContext puctx)
+        {
+            _puctx = puctx;
+        }
+
+        public IActionResult OnGet(string path)
         {
             FileResource = new FileResource
             {
                 Name = Path.GetFileName(path),
                 Path = path
             };
+
+            if (!System.IO.File.Exists(path))
+            {
+                ModelState.AddModelError("", "文件不存在");
+            }
+
+            var fileInfo = new FileInfo(path);
+            FileResource.Size = fileInfo.Length;
+            return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(string path)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return Page();
             }
 
+            if (!System.IO.File.Exists(path))
+            {
+                ModelState.AddModelError("", "文件不存在");
+                return Page();
+            }
+
+            var fileInfo = new FileInfo(path);
             FileResource.Path = path;
-            //save file
-            await Task.CompletedTask;
+            FileResource.Size = fileInfo.Length;
+
+            _puctx.FileResource.Add(FileResource);
+            await _puctx.SaveChangesAsync();
+            
             return RedirectToPage("./ResList");
         }
     }

1ed9d0c9e2055281044013d9f3a0bcaa216f076d
chunanshan ma <machuanshan@gmail.com>
2020-10-11 16:54:53 +0800

    add title

--- ParallelUniverse.RazorPages/Pages/VideoStream.cshtml.cs
+++ ParallelUniverse.RazorPages/Pages/VideoStream.cshtml.cs
@@ -5,13 +5,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
 namespace ParallelUniverse.RazorPages.Pages
 {
     public class VideoStreamModel : PageModel
     {
+        private readonly ILogger<VideoStreamModel> _logger;
+
+        public VideoStreamModel(ILogger<VideoStreamModel> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult OnGet(string file)
         {
+            _logger.LogDebug($"Send file: {file}");
+
             return new PhysicalFileResult(file, "application/octet-stream")
             {
                 FileDownloadName = Path.GetFileName(file),
